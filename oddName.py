aed0ee0b20abba63692662efcc88b04e37f8a96f
alexvonkurtz <alexander.vonkurtz@gmail.com>
2016-08-24 17:28:29 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,4 +1,4 @@
-userName = input("Please enter your name " )
+userName = input("Please enter your name" )
 while userName == "":
     print ("You did not enter anything!")
     userName = input("Please enter your name ")
